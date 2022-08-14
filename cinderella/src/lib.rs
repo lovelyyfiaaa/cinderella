0a024c300c3cef55b5375df0d7da81ae551e5b58
Nefo Fortressia <nefothingy@hotmail.com>
2022-08-14 20:17:36 +0700

    Remove default tests in cinderella
    Signed-off-by: Nefo Fortressia <nefothingy@hotmail.com>

--- cinderella/src/lib.rs
+++ cinderella/src/lib.rs
@@ -3,11 +3,5 @@ pub use embedded::terminal;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+  
 }
